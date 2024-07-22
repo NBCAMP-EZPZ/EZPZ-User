74a9dfbed373657f388a7fbcef987afbdbdee789
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-22 11:27:31 +0900

    init commit

---
+++ src/main/java/com/sparta/ezpzuser/EzpzUserApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ezpzuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzpzUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzpzUserApplication.class, args);
+    }
+
+}
