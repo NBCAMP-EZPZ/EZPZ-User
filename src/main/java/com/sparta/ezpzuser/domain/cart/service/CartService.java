96165d2608837e74bda9d5b2804f205feeee311c
M1ngD0ng <alswjd6807@naver.com>
2024-07-23 22:04:07 +0900

    Refactor [#2] : 주석 추가 및 중복된 코드 처리

--- src/main/java/com/sparta/ezpzuser/domain/cart/service/CartService.java
+++ src/main/java/com/sparta/ezpzuser/domain/cart/service/CartService.java
@@ -27,6 +27,13 @@ public class CartService {
     private final CartRepository cartRepository;
     private final UserRepository userRepository;
 
+    /**
+     * 장바구니 생성
+     *
+     * @param requestDto 생성할 장바구니 정보
+     * @param user       요청 사용자
+     * @return 생성된 장바구니 정보
+     */
     @Transactional
     public CartResponseDto createCart(CartCreateRequestDto requestDto, User user) {
         Item item = itemRepository.findById(requestDto.getItemId()).orElseThrow(
@@ -47,6 +54,12 @@ public class CartService {
         return CartResponseDto.of(cart);
     }
 
+    /**
+     * 장바구니 내역 조회
+     *
+     * @param user 요청한 사용자
+     * @return 장바구니 내역 리스트
+     */
     public List<CartResponseDto> findCartsAll(User user) {
         List<Cart> cartList = cartRepository.findAllByUserIdOrderByCreatedAtDesc(
                 user.getId());
@@ -60,15 +73,17 @@ public class CartService {
         return cartList.stream().map(CartResponseDto::of).collect(Collectors.toList());
     }
 
+    /**
+     * 장바구니 수량 변경
+     *
+     * @param cartId     수량을 변경할 장바구니의 id
+     * @param requestDto 변경할 수량
+     * @param user       요청한 사용자
+     * @return 변경된 장바구니의 정보
+     */
     @Transactional
     public CartResponseDto updateCart(Long cartId, CartUpdateRequestDto requestDto, User user) {
-        Cart cart = cartRepository.findById(cartId)
-                .orElseThrow(() -> new CustomException(ErrorType.CART_NOT_FOUND));
-
-        // 본인의 장바구니가 아닌데 수정하려고 할 때의 예외 처리
-        if (!user.getId().equals(cart.getUser().getId())) {
-            throw new CustomException(ErrorType.UNAUTHORIZED_CART_ACCESS);
-        }
+        Cart cart = getValidatedCart(cartId, user);
 
         Item item = itemRepository.findById(cart.getItem().getId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다."));
@@ -82,8 +97,28 @@ public class CartService {
         return CartResponseDto.of(cart);
     }
 
+    /**
+     * 장바구니 삭제
+     *
+     * @param cartId 삭제할 장바구니 id
+     * @param user   요청한 사용자
+     */
     @Transactional
     public void deleteCart(Long cartId, User user) {
+        Cart cart = getValidatedCart(cartId, user);
+        cartRepository.delete(cart);
+    }
+
+    /* UTIL*/
+
+    /**
+     * 해당 장바구니가 존재하고, 요청한 사용자의 장바구니가 맞는지 확인하는 메서드
+     *
+     * @param cartId 장바구니 id
+     * @param user   요청한 사용자
+     * @return 존재하는 장바구니
+     */
+    private Cart getValidatedCart(Long cartId, User user) {
         Cart cart = cartRepository.findById(cartId)
                 .orElseThrow(() -> new CustomException(ErrorType.CART_NOT_FOUND));
 
@@ -91,6 +126,7 @@ public class CartService {
         if (!user.getId().equals(cart.getUser().getId())) {
             throw new CustomException(ErrorType.UNAUTHORIZED_CART_ACCESS);
         }
-        cartRepository.delete(cart);
+
+        return cart;
     }
 }
