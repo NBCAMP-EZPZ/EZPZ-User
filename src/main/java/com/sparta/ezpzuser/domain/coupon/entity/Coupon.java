59a18b793bd98b400c1c6e3b754113583e4a5370
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-24 17:06:52 +0900

    Feat [#11] : Add Coupon entity

---
+++ src/main/java/com/sparta/ezpzuser/domain/coupon/entity/Coupon.java
@@ -0,0 +1,45 @@
+package com.sparta.ezpzuser.domain.coupon.entity;
+
+import com.sparta.ezpzuser.common.exception.CustomException;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+import static com.sparta.ezpzuser.common.exception.ErrorType.SOLD_OUT_COUPON;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coupon_id")
+    private Long id;
+
+    private String name;
+
+    private int discountAmount;
+
+    private int totalCount; // 총 발행 개수
+
+    private int remainingCount; // 남은 수량
+
+    private LocalDate expiredAt;
+
+    /**
+     * 쿠폰 다운로드
+     */
+    public void download() {
+        // 남은 수량이 있는지 확인
+        if (this.remainingCount == 0) {
+            throw new CustomException(SOLD_OUT_COUPON);
+        }
+        // 남은 수량 감소
+        remainingCount--;
+    }
+
+}
