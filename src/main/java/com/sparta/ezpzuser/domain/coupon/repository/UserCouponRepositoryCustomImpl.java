cd12983b33722748e84b2daed45ccfac73d7e2d5
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-24 17:08:10 +0900

    Feat [#11] : Add UserCoupon Repositories

---
+++ src/main/java/com/sparta/ezpzuser/domain/coupon/repository/UserCouponRepositoryCustomImpl.java
@@ -0,0 +1,50 @@
+package com.sparta.ezpzuser.domain.coupon.repository;
+
+import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.ezpzuser.domain.coupon.entity.Coupon;
+import com.sparta.ezpzuser.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.sparta.ezpzuser.common.util.RepositoryUtil.getTotal;
+import static com.sparta.ezpzuser.domain.coupon.entity.QCoupon.coupon;
+import static com.sparta.ezpzuser.domain.coupon.entity.QUserCoupon.userCoupon;
+
+@Repository
+@RequiredArgsConstructor
+public class UserCouponRepositoryCustomImpl implements UserCouponRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<Coupon> findAllMyCoupons(User user, Pageable pageable) {
+        // 데이터 조회 쿼리
+        List<Coupon> coupons = queryFactory
+                .select(coupon)
+                .from(userCoupon)
+                .where(
+                        userCoupon.user.eq(user)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 전체 카운트 쿼리
+        Long totalCount = queryFactory
+                .select(Wildcard.count)
+                .from(userCoupon)
+                .where(
+                        userCoupon.user.eq(user)
+                )
+                .fetchOne();
+
+        return new PageImpl<>(coupons, pageable, getTotal(totalCount));
+    }
+
+}
