3c319f2ff87e527b629eb929593d56bdf54f1d07
llocr <36-96@naver.com>
2024-07-29 16:16:05 +0900

    Test [#36] : Index 적용

--- src/main/java/com/sparta/ezpzuser/domain/reservation/entity/Reservation.java
+++ src/main/java/com/sparta/ezpzuser/domain/reservation/entity/Reservation.java
@@ -13,8 +13,10 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +24,9 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "reservation", indexes = {
+	@Index(name = "idx_reservation_user_status", columnList = "user_id, reservation_status, slot_id")
+})
 public class Reservation extends Timestamped {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
