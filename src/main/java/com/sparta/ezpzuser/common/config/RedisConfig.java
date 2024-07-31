a860b6fbd87bdae4523565adb0958b7c7e327bdb
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-31 21:31:35 +0900

    Refactor [#27] : Update application.yml

--- src/main/java/com/sparta/ezpzuser/common/config/RedisConfig.java
+++ src/main/java/com/sparta/ezpzuser/common/config/RedisConfig.java
@@ -1,32 +1,49 @@
 package com.sparta.ezpzuser.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.ClusterServersConfig;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class RedisConfig {
+@RequiredArgsConstructor
+public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
+    @Value("${spring.data.redis.mode}")
+    private String mode;
 
     @Value("${spring.data.redis.password}")
     private String password;
 
-    private static final String REDISSON_PREFIX = "redis://";
+    private final RedisClusterProperties properties;
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer()
-                .setAddress(REDISSON_PREFIX + host + ":" + port)
-                .setPassword(password);
+
+        if (mode.equals("single")) {
+            config.useSingleServer()
+                    .setAddress("redis://127.0.0.1:6379")
+                    .setPassword(password);
+        } else if (mode.equals("cluster")) {
+            System.out.println("---------------------------RedisConfig.redissonClient");
+            ClusterServersConfig clusterServersConfig = config.useClusterServers().setPassword(password);
+            properties.getNodes().forEach(clusterServersConfig::addNodeAddress);
+//            config.useClusterServers().setPassword(password)
+//                    .addNodeAddress("redis://127.0.0.1:6000")
+//                    .addNodeAddress("redis://127.0.0.1:6001")
+//                    .addNodeAddress("redis://127.0.0.1:6002")
+//                    .addNodeAddress("redis://127.0.0.1:6003")
+//                    .addNodeAddress("redis://127.0.0.1:6004")
+//                    .addNodeAddress("redis://127.0.0.1:6005");
+        } else {
+            throw new IllegalArgumentException("Redisson mode not recognized");
+        }
+
         return Redisson.create(config);
     }
 
