641a90eb426586b37972c0c4cf73674ed5556a66
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-02 21:34:25 +0900

    Refactor [#51] : 메서드명 변경

--- src/main/java/com/sparta/ezpzuser/domain/item/repository/ItemRepositoryCustom.java
+++ src/main/java/com/sparta/ezpzuser/domain/item/repository/ItemRepositoryCustom.java
@@ -5,11 +5,12 @@ import com.sparta.ezpzuser.domain.item.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.nio.channels.FileChannel;
 import java.util.List;
 
 public interface ItemRepositoryCustom {
-    Page<Item> findAllItemsByHostAndPopupAndStatus(Pageable pageable, ItemCondition cond);
+
+    Page<Item> findAllByItemCondition(Pageable pageable, ItemCondition cond);
 
     Page<Item> findItemByIdList(Pageable pageable, List<Long> itemIdList);
+
 }
