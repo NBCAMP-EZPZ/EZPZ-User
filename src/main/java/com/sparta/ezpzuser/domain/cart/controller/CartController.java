27356a409c7afc8fc1332b2fb8f4d20486b16b1e
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-02 21:21:05 +0900

    Refactor [#51] : CartController 가독성 개선

--- src/main/java/com/sparta/ezpzuser/domain/cart/controller/CartController.java
+++ src/main/java/com/sparta/ezpzuser/domain/cart/controller/CartController.java
@@ -1,25 +1,21 @@
 package com.sparta.ezpzuser.domain.cart.controller;
 
 
-import static com.sparta.ezpzuser.common.util.ControllerUtil.getResponseEntity;
-
 import com.sparta.ezpzuser.common.dto.CommonResponse;
 import com.sparta.ezpzuser.common.security.UserDetailsImpl;
 import com.sparta.ezpzuser.domain.cart.dto.CartCreateRequestDto;
+import com.sparta.ezpzuser.domain.cart.dto.CartResponseDto;
 import com.sparta.ezpzuser.domain.cart.dto.CartUpdateRequestDto;
 import com.sparta.ezpzuser.domain.cart.service.CartService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.sparta.ezpzuser.common.util.ControllerUtil.getResponseEntity;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,14 +28,17 @@ public class CartController {
     public ResponseEntity<CommonResponse<?>> createCart(
             @Valid @RequestBody CartCreateRequestDto requestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return getResponseEntity(cartService.createCart(requestDto, userDetails.getUser()),
-                "장바구니 추가 성공");
+
+        CartResponseDto response = cartService.createCart(requestDto, userDetails.getUser());
+        return getResponseEntity(response, "장바구니 추가 성공");
     }
 
     @GetMapping
     public ResponseEntity<CommonResponse<?>> findCartsAll(
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return getResponseEntity(cartService.findCartsAll(userDetails.getUser()), "장바구니 조회 성공");
+
+        List<CartResponseDto> response = cartService.findCartsAll(userDetails.getUser());
+        return getResponseEntity(response, "장바구니 조회 성공");
     }
 
     @PatchMapping("/{cartId}")
@@ -47,15 +46,18 @@ public class CartController {
             @PathVariable("cartId") Long cartId,
             @Valid @RequestBody CartUpdateRequestDto requestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return getResponseEntity(cartService.updateCart(cartId, requestDto, userDetails.getUser()),
-                "장바구니 수량 변경 성공");
+
+        CartResponseDto response = cartService.updateCart(cartId, requestDto, userDetails.getUser());
+        return getResponseEntity(response, "장바구니 수량 변경 성공");
     }
 
     @DeleteMapping("/{cartId}")
     public ResponseEntity<CommonResponse<?>> deleteCart(
             @PathVariable("cartId") Long cartId,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
         cartService.deleteCart(cartId, userDetails.getUser());
         return getResponseEntity("장바구니 삭제 성공");
     }
+
 }
