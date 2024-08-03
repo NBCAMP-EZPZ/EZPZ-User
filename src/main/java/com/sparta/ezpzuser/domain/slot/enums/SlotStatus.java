41e8aeffeae71c13c5c4a8476aabcd59619bac46
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-03 21:20:26 +0900

    Refactor [#51] : 주석 수정

--- src/main/java/com/sparta/ezpzuser/domain/slot/enums/SlotStatus.java
+++ src/main/java/com/sparta/ezpzuser/domain/slot/enums/SlotStatus.java
@@ -1,10 +1,9 @@
 package com.sparta.ezpzuser.domain.slot.enums;
 
 public enum SlotStatus {
-	// 진행 예정
-	READY,
-	// 진행 중
-	PROCEEDING,
-	// 진행 완료
-	FINISHED
+
+    READY, // 진행 전
+    PROCEEDING, // 진행 중
+    FINISHED // 진행 완료
+
 }
