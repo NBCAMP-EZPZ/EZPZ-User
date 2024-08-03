5bd3c0aa1a92139427bf1465a0c5fafdafd79fd9
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-03 21:16:27 +0900

    Refactor [#51] : 들여쓰기 수정

--- src/main/java/com/sparta/ezpzuser/domain/reservation/repository/ReservationRepositoryCustomImpl.java
+++ src/main/java/com/sparta/ezpzuser/domain/reservation/repository/ReservationRepositoryCustomImpl.java
@@ -1,86 +1,86 @@
 package com.sparta.ezpzuser.domain.reservation.repository;
 
-import static com.sparta.ezpzuser.common.util.RepositoryUtil.getTotal;
-import static com.sparta.ezpzuser.domain.reservation.entity.QReservation.reservation;
-
-import java.util.List;
-import java.util.Optional;
-
+import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.ezpzuser.domain.reservation.entity.Reservation;
+import com.sparta.ezpzuser.domain.reservation.enums.ReservationStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import com.querydsl.core.types.dsl.Wildcard;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.sparta.ezpzuser.domain.reservation.entity.Reservation;
-import com.sparta.ezpzuser.domain.reservation.enums.ReservationStatus;
+import java.util.List;
+import java.util.Optional;
 
-import lombok.RequiredArgsConstructor;
+import static com.sparta.ezpzuser.common.util.RepositoryUtil.getTotal;
+import static com.sparta.ezpzuser.domain.reservation.entity.QReservation.reservation;
 
 @Repository
 @RequiredArgsConstructor
 public class ReservationRepositoryCustomImpl implements ReservationRepositoryCustom {
-	private final JPAQueryFactory queryFactory;
-	
-	@Override
-	public boolean existsByUserIdAndPopupId(Long userId, Long popupId) {
-		return queryFactory
-			.selectOne()
-			.from(reservation)
-			.where(reservation.user.id.eq(userId)
-				.and(reservation.reservationStatus.ne(ReservationStatus.CANCEL))
-				.and(reservation.slot.popup.id.eq(popupId)))
-			.fetchFirst() != null;
-	}
-	
-	@Override
-	public Page<Reservation> findByUserIdAndStatus(Long userId, ReservationStatus status, Pageable pageable) {
-		List<Reservation> reservations = queryFactory
-			.selectFrom(reservation)
-			.join(reservation.slot).fetchJoin()
-			.join(reservation.slot.popup).fetchJoin()
-			.where(reservation.user.id.eq(userId)
-				.and(reservation.reservationStatus.eq(status)))
-			.orderBy(reservation.slot.slotDate.desc(), reservation.slot.slotTime.desc())
-			.offset(pageable.getOffset())
-			.limit(pageable.getPageSize())
-			.fetch();
-		
-		Long totalCount = queryFactory
-			.select(Wildcard.count)
-			.from(reservation)
-			.where(reservation.user.id.eq(userId)
-				.and(reservation.reservationStatus.eq(status)))
-			.fetchOne();
-		
-		return new PageImpl<>(reservations, pageable, getTotal(totalCount));
-	}
-	
-	@Override
-	public Optional<Reservation> findReservationAndSlot(Long reservationId, Long userId) {
-		Reservation findReservation = queryFactory
-			.selectFrom(reservation)
-			.join(reservation.slot).fetchJoin()
-			.where(reservation.id.eq(reservationId)
-				.and(reservation.user.id.eq(userId)))
-			.fetchOne();
-		
-		return Optional.ofNullable(findReservation);
-	}
-	
-	@Override
-	public Optional<Reservation> findReservationAndSlotPopUp(Long reservationId, Long userId) {
-		Reservation findReservation = queryFactory
-			.selectFrom(reservation)
-			.join(reservation.slot).fetchJoin()
-			.join(reservation.slot.popup).fetchJoin()
-			.where(reservation.id.eq(reservationId)
-				.and(reservation.user.id.eq(userId)))
-			.fetchOne();
-		
-		return Optional.ofNullable(findReservation);
-		
-		
-	}
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public boolean existsByUserIdAndPopupId(Long userId, Long popupId) {
+        return queryFactory
+                .selectOne()
+                .from(reservation)
+                .where(reservation.user.id.eq(userId)
+                        .and(reservation.reservationStatus.ne(ReservationStatus.CANCELED))
+                        .and(reservation.slot.popup.id.eq(popupId)))
+                .fetchFirst() != null;
+    }
+
+    @Override
+    public Page<Reservation> findByUserIdAndStatus(Long userId, ReservationStatus status, Pageable pageable) {
+        List<Reservation> reservations = queryFactory
+                .selectFrom(reservation)
+                .join(reservation.slot).fetchJoin()
+                .join(reservation.slot.popup).fetchJoin()
+                .where(reservation.user.id.eq(userId)
+                        .and(reservation.reservationStatus.eq(status)))
+                .orderBy(reservation.slot.slotDate.desc(), reservation.slot.slotTime.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long totalCount = queryFactory
+                .select(Wildcard.count)
+                .from(reservation)
+                .where(reservation.user.id.eq(userId)
+                        .and(reservation.reservationStatus.eq(status)))
+                .fetchOne();
+
+        return new PageImpl<>(reservations, pageable, getTotal(totalCount));
+    }
+
+    @Override
+    public Optional<Reservation> findReservationAndSlot(Long reservationId, Long userId) {
+        Reservation findReservation = queryFactory
+                .selectFrom(reservation)
+                .join(reservation.slot).fetchJoin()
+                .where(reservation.id.eq(reservationId)
+                        .and(reservation.user.id.eq(userId)))
+                .fetchOne();
+
+        return Optional.ofNullable(findReservation);
+    }
+
+    @Override
+    public Optional<Reservation> findReservationAndSlotPopUp(Long reservationId, Long userId) {
+        Reservation findReservation = queryFactory
+                .selectFrom(reservation)
+                .join(reservation.slot).fetchJoin()
+                .join(reservation.slot.popup).fetchJoin()
+                .where(reservation.id.eq(reservationId)
+                        .and(reservation.user.id.eq(userId)))
+                .fetchOne();
+
+        return Optional.ofNullable(findReservation);
+
+
+    }
+
 }
