fda463b2286af21f4433888a44735371a95ca9d3
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-03 21:21:51 +0900

    Refactor [#51] : 들여쓰기 수정

--- src/main/java/com/sparta/ezpzuser/domain/order/enums/OrderStatus.java
+++ src/main/java/com/sparta/ezpzuser/domain/order/enums/OrderStatus.java
@@ -1,8 +1,10 @@
 package com.sparta.ezpzuser.domain.order.enums;
 
 public enum OrderStatus {
+
     ORDER_COMPLETED,   // 주문 완료
     IN_TRANSIT,        // 배송 중
     DELIVERED,         // 배송 완료
     CANCELLED          // 주문 취소
+
 }
