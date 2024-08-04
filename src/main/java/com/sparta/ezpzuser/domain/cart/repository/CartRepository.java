a35515d7859fd3b4a8e288b10d91700fbcf5f5a7
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-04 20:39:21 +0900

    Refactor [#51] : 메서드명 수정

--- src/main/java/com/sparta/ezpzuser/domain/cart/repository/CartRepository.java
+++ src/main/java/com/sparta/ezpzuser/domain/cart/repository/CartRepository.java
@@ -1,10 +1,11 @@
 package com.sparta.ezpzuser.domain.cart.repository;
 
 import com.sparta.ezpzuser.domain.cart.entity.Cart;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
@@ -12,6 +13,7 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findAllByUserIdOrderByCreatedAtDesc(Long userId);
 
     // item까지 함께 가져오도록 구현
-    @Query("SELECT c FROM Cart c JOIN FETCH c.item WHERE c.id IN :cartIds")
-    List<Cart> findAllByIdWithItems(@Param("cartIds") List<Long> cartIds);
+    @Query("SELECT c FROM Cart c JOIN FETCH c.item WHERE c.id IN :cartIdList")
+    List<Cart> findAllByIdList(@Param("cartIdList") List<Long> cartIdList);
+
 }
