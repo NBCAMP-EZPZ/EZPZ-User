a4136f95ab9b05f29702a9096cc4a1bbcb4f85b0
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-08-08 16:22:34 +0900

    Update OrderServiceConcurrencyTest.java

--- src/test/java/com/sparta/ezpzuser/domain/order/service/OrderServiceConcurrencyTest.java
+++ src/test/java/com/sparta/ezpzuser/domain/order/service/OrderServiceConcurrencyTest.java
@@ -1,86 +1,86 @@
-package com.sparta.ezpzuser.domain.order.service;
+// package com.sparta.ezpzuser.domain.order.service;
 
-import com.sparta.ezpzuser.domain.item.repository.ItemRepository;
-import com.sparta.ezpzuser.domain.order.dto.OrderRequestDto;
-import com.sparta.ezpzuser.domain.order.dto.OrderRequestDto.CartIdRequest;
-import com.sparta.ezpzuser.domain.order.dto.OrderResponseDto;
-import com.sparta.ezpzuser.domain.user.entity.User;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.transaction.annotation.Transactional;
+// import com.sparta.ezpzuser.domain.item.repository.ItemRepository;
+// import com.sparta.ezpzuser.domain.order.dto.OrderRequestDto;
+// import com.sparta.ezpzuser.domain.order.dto.OrderRequestDto.CartIdRequest;
+// import com.sparta.ezpzuser.domain.order.dto.OrderResponseDto;
+// import com.sparta.ezpzuser.domain.user.entity.User;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.concurrent.CountDownLatch;
+// import java.util.concurrent.ExecutorService;
+// import java.util.concurrent.Executors;
+// import java.util.concurrent.atomic.AtomicInteger;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.annotation.Rollback;
+// import org.springframework.test.context.TestPropertySource;
+// import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@Transactional
-@TestPropertySource(locations = "classpath:properties/env.properties")
-//@ActiveProfiles("test")
-public class OrderServiceConcurrencyTest {
+// @SpringBootTest
+// @Transactional
+// @TestPropertySource(locations = "classpath:properties/env.properties")
+// //@ActiveProfiles("test")
+// public class OrderServiceConcurrencyTest {
 
-    @Autowired
-    private OrderService orderService;
+//     @Autowired
+//     private OrderService orderService;
 
-    @Autowired
-    private ItemRepository itemRepository;
+//     @Autowired
+//     private ItemRepository itemRepository;
 
-    private User user;
+//     private User user;
 
-    @BeforeEach
-    public void setUp() {
-        user = new User(
-                2003L,
-                "useruseraa1",
-                "$2a$10$IDFtfqng8SA4PdbJi.2UMur1OnmWQloswZ7mQaZSh3zBYSCRwNpmW",
-                "user1",
-                "user1@naver.com",
-                "01011111111",
-                new ArrayList<>()
-        );
-    }
+//     @BeforeEach
+//     public void setUp() {
+//         user = new User(
+//                 2003L,
+//                 "useruseraa1",
+//                 "$2a$10$IDFtfqng8SA4PdbJi.2UMur1OnmWQloswZ7mQaZSh3zBYSCRwNpmW",
+//                 "user1",
+//                 "user1@naver.com",
+//                 "01011111111",
+//                 new ArrayList<>()
+//         );
+//     }
 
-    @Test
-    @DisplayName("Test 동시성 주문 생성")
-    @Rollback
-    void testConcurrentOrderCreation() throws InterruptedException {
-        int userCount = 100;
-        int baseCartId = 50099;
+//     @Test
+//     @DisplayName("Test 동시성 주문 생성")
+//     @Rollback
+//     void testConcurrentOrderCreation() throws InterruptedException {
+//         int userCount = 100;
+//         int baseCartId = 50099;
 
-        AtomicInteger successCount = new AtomicInteger();
-        AtomicInteger failCount = new AtomicInteger();
+//         AtomicInteger successCount = new AtomicInteger();
+//         AtomicInteger failCount = new AtomicInteger();
 
-        ExecutorService executorService = Executors.newFixedThreadPool(userCount);
-        CountDownLatch latch = new CountDownLatch(userCount);
+//         ExecutorService executorService = Executors.newFixedThreadPool(userCount);
+//         CountDownLatch latch = new CountDownLatch(userCount);
 
-        for (int i = 1; i <= userCount; i++) {
-            final int cartId = baseCartId + i - 1;
-            executorService.submit(() -> {
-                try {
-                    OrderRequestDto requestDto = new OrderRequestDto(
-                            List.of(new CartIdRequest((long) cartId))
-                    );
+//         for (int i = 1; i <= userCount; i++) {
+//             final int cartId = baseCartId + i - 1;
+//             executorService.submit(() -> {
+//                 try {
+//                     OrderRequestDto requestDto = new OrderRequestDto(
+//                             List.of(new CartIdRequest((long) cartId))
+//                     );
 
-                    OrderResponseDto responseDto = orderService.createOrder(requestDto, user);
-                    successCount.incrementAndGet();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    failCount.incrementAndGet();
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
-        latch.await();
+//                     OrderResponseDto responseDto = orderService.createOrder(requestDto, user);
+//                     successCount.incrementAndGet();
+//                 } catch (Exception e) {
+//                     System.out.println(e.getMessage());
+//                     failCount.incrementAndGet();
+//                 } finally {
+//                     latch.countDown();
+//                 }
+//             });
+//         }
+//         latch.await();
 
-        System.out.println("successCount = " + successCount);
-        System.out.println("failCount = " + failCount);
-    }
-}
+//         System.out.println("successCount = " + successCount);
+//         System.out.println("failCount = " + failCount);
+//     }
+// }
