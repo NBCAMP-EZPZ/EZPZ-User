774569a21b677412a87cbbaa8565601dd5c2fb32
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-08 12:26:17 +0900

    Refactor [#78] : CouponResponseDto totalCount 추가 및 couponId 필드 수정

--- src/main/java/com/sparta/ezpzuser/domain/coupon/dto/CouponResponseDto.java
+++ src/main/java/com/sparta/ezpzuser/domain/coupon/dto/CouponResponseDto.java
@@ -8,15 +8,17 @@ import java.time.LocalDate;
 @Getter
 public class CouponResponseDto {
 
-    private final Long id;
+    private final Long couponId;
     private final String name;
     private final int discountAmount;
+    private final int totalCount;
     private final LocalDate expiredAt;
 
     private CouponResponseDto(Coupon coupon) {
-        this.id = coupon.getId();
+        this.couponId = coupon.getId();
         this.name = coupon.getName();
         this.discountAmount = coupon.getDiscountAmount();
+        this.totalCount = coupon.getTotalCount();
         this.expiredAt = coupon.getExpiredAt();
     }
 
