f9609da028fa99d0de113887da083c8a743f41e2
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-09 16:32:40 +0900

    Refactor [#78] : SlotResponseDto 내부 필드명 수정

--- src/main/java/com/sparta/ezpzuser/domain/slot/dto/SlotResponseDto.java
+++ src/main/java/com/sparta/ezpzuser/domain/slot/dto/SlotResponseDto.java
@@ -1,26 +1,28 @@
 package com.sparta.ezpzuser.domain.slot.dto;
 
 import com.sparta.ezpzuser.domain.slot.entity.Slot;
-
+import com.sparta.ezpzuser.domain.slot.enums.SlotStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class SlotResponseDto {
-	private Long id;
-	private String slotDate;
-	private String slotTime;
-	private String slotStatus;
-	
-	private SlotResponseDto(Slot slot) {
-		this.id = slot.getId();
-		this.slotDate = slot.getSlotDate().toString();
-		this.slotTime = slot.getSlotTime().toString();
-		this.slotStatus = slot.getSlotStatus().toString();
-	}
-	
-	public static SlotResponseDto of(Slot slot) {
-		return new SlotResponseDto(slot);
-	}
+
+    private Long slotId;
+    private SlotStatus slotStatus;
+    private String slotDate;
+    private String slotTime;
+
+    private SlotResponseDto(Slot slot) {
+        this.slotId = slot.getId();
+        this.slotDate = slot.getSlotDate().toString();
+        this.slotTime = slot.getSlotTime().toString();
+        this.slotStatus = slot.getSlotStatus();
+    }
+
+    public static SlotResponseDto of(Slot slot) {
+        return new SlotResponseDto(slot);
+    }
+
 }
