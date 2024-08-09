c21b6ee739cf53bae177a262606396e027d36713
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-09 16:12:49 +0900

    Refactor [#78] : 팝업 관련 DTO 내부 필드명 수정

--- src/main/java/com/sparta/ezpzuser/domain/popup/dto/PopupPageResponseDto.java
+++ src/main/java/com/sparta/ezpzuser/domain/popup/dto/PopupPageResponseDto.java
@@ -1,28 +1,30 @@
 package com.sparta.ezpzuser.domain.popup.dto;
 
 import com.sparta.ezpzuser.domain.popup.entity.Popup;
+import com.sparta.ezpzuser.domain.popup.enums.PopupStatus;
 import lombok.Getter;
 
 @Getter
 public class PopupPageResponseDto {
 
     private final Long popupId;
-    private final String name;
+    private final PopupStatus popupStatus;
+    private final String popupName;
     private final int likeCount;
-    private final String thumbnailUrl;
-    private final String popupStatus;
     private final String companyName;
+    private final String thumbnailUrl;
 
     public PopupPageResponseDto(Popup popup) {
         this.popupId = popup.getId();
-        this.name = popup.getName();
+        this.popupName = popup.getName();
         this.likeCount = popup.getLikeCount();
         this.thumbnailUrl = popup.getThumbnailUrl();
-        this.popupStatus = popup.getPopupStatus().toString();
+        this.popupStatus = popup.getPopupStatus();
         this.companyName = popup.getHost().getCompanyName();
     }
 
     public static PopupPageResponseDto of(Popup popup) {
         return new PopupPageResponseDto(popup);
     }
+
 }
