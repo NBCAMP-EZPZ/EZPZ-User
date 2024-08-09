0170f5a780f840bc19391b422ffee41c6a49dc42
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-09 16:33:47 +0900

    Refactor [#78] : querydsl로 작성된 부분 spring data jpa 쿼리 메서드로 리팩토링

--- src/main/java/com/sparta/ezpzuser/domain/slot/repository/SlotRepository.java
+++ src/main/java/com/sparta/ezpzuser/domain/slot/repository/SlotRepository.java
@@ -1,7 +1,12 @@
 package com.sparta.ezpzuser.domain.slot.repository;
 
 import com.sparta.ezpzuser.domain.slot.entity.Slot;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SlotRepository extends JpaRepository<Slot, Long>, SlotRepositoryCustom {
+
+    Page<Slot> findAllByPopupId(Long popupId, Pageable pageable);
+
 }
--- src/main/java/com/sparta/ezpzuser/domain/slot/repository/SlotRepositoryCustom.java
+++ src/main/java/com/sparta/ezpzuser/domain/slot/repository/SlotRepositoryCustom.java
@@ -1,15 +1,11 @@
 package com.sparta.ezpzuser.domain.slot.repository;
 
 import com.sparta.ezpzuser.domain.slot.entity.Slot;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface SlotRepositoryCustom {
 
     Optional<Slot> findSlotByIdWithPopup(Long slotId);
-
-    Page<Slot> findByPopupId(Long popupId, Pageable pageable);
 
 }
