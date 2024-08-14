39035fe113cc9ada239be310afc8bfec032749f2
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-14 11:38:53 +0900

    Refactor [#78] : 예약 동시성 테스트 리팩토링

---
+++ src/test/java/com/sparta/ezpzuser/domain/reservation/ReservationConcurrencyTest.java
@@ -0,0 +1,78 @@
+package com.sparta.ezpzuser.domain.reservation;
+
+import com.sparta.ezpzuser.domain.popup.entity.Popup;
+import com.sparta.ezpzuser.domain.popup.repository.popup.PopupRepository;
+import com.sparta.ezpzuser.domain.reservation.dto.ReservationRequestDto;
+import com.sparta.ezpzuser.domain.reservation.repository.ReservationRepository;
+import com.sparta.ezpzuser.domain.reservation.service.ReservationService;
+import com.sparta.ezpzuser.domain.slot.entity.Slot;
+import com.sparta.ezpzuser.domain.slot.enums.SlotStatus;
+import com.sparta.ezpzuser.domain.slot.repository.SlotRepository;
+import com.sparta.ezpzuser.domain.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+public class ReservationConcurrencyTest {
+
+    @Autowired
+    ReservationService reservationService;
+
+    @MockBean
+    ReservationRepository reservationRepository;
+
+    @Autowired
+    PopupRepository popupRepository;
+
+    @Autowired
+    SlotRepository slotRepository;
+
+    User user = User.createMockUser();
+
+    Popup popup;
+
+    Slot slot;
+
+    int threadCount = 100;
+
+    int availableCount = 2;
+
+    int totalCount = threadCount * availableCount;
+
+    @BeforeEach
+    void setUp() {
+        popup = Popup.createMockPopup();
+        popupRepository.save(popup);
+
+        slot = Slot.createMockSlot(availableCount, totalCount, popup, SlotStatus.PROCEEDING);
+        slotRepository.save(slot);
+    }
+
+    @Test
+    public void 예약_동시성_테스트() {
+        // given
+        given(reservationRepository.existsByUserIdAndPopupId(anyLong(), anyLong()))
+                .willReturn(false);
+        Long slotId = slot.getId();
+
+        // when
+        ReservationRequestDto requestDto = new ReservationRequestDto(slotId, availableCount);
+        IntStream.range(0, threadCount).parallel().forEach(i ->
+                reservationService.createReservation(requestDto, user)
+        );
+
+        // then
+        int reservedCount = slotRepository.findById(slotId).orElseThrow().getReservedCount();
+        assertThat(reservedCount).isEqualTo(totalCount);
+    }
+
+}
