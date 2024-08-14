399cad0e6befa50022960835e05111ddd9dfe3b2
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-14 14:20:39 +0900

    Refactor [#78] : NoArgsConstructor 추가

--- src/main/java/com/sparta/ezpzuser/domain/order/dto/OrderRequestDto.java
+++ src/main/java/com/sparta/ezpzuser/domain/order/dto/OrderRequestDto.java
@@ -2,10 +2,12 @@ package com.sparta.ezpzuser.domain.order.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class OrderRequestDto {
 
     @NotEmpty(message = "최소 1개 이상의 장바구니를 선택해주세요.")
