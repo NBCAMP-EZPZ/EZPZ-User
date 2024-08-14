9f47bd30a1dc1e4d9c8d8c0973aa22a07a810ead
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-08-14 11:37:45 +0900

    Refactor [#78] : 좋아요 동시성 테스트 리팩토링

---
+++ src/test/java/com/sparta/ezpzuser/domain/like/LikeConcurrencyTest.java
@@ -0,0 +1,67 @@
+package com.sparta.ezpzuser.domain.like;
+
+import com.sparta.ezpzuser.domain.like.dto.LikeRequestDto;
+import com.sparta.ezpzuser.domain.like.entity.LikeContentType;
+import com.sparta.ezpzuser.domain.like.repository.LikeRepository;
+import com.sparta.ezpzuser.domain.like.service.LikeService;
+import com.sparta.ezpzuser.domain.popup.entity.Popup;
+import com.sparta.ezpzuser.domain.popup.repository.popup.PopupRepository;
+import com.sparta.ezpzuser.domain.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+public class LikeConcurrencyTest {
+
+    @Autowired
+    LikeService likeService;
+
+    @Autowired
+    PopupRepository popupRepository;
+
+    @MockBean
+    LikeRepository likeRepository;
+
+    User user = User.createMockUser();
+
+    Popup popup;
+
+    int threadCount = 100;
+
+    @BeforeEach
+    void setUp() {
+        popup = Popup.createMockPopup();
+        popupRepository.save(popup);
+    }
+
+    @Test
+    void 좋아요_동시성_테스트() {
+        // given
+        given(likeRepository.findByContentTypeAndContentIdAndUser(any(LikeContentType.class), anyLong(), any(User.class)))
+                .willReturn(Optional.empty());
+        LikeContentType contentType = LikeContentType.POPUP;
+        Long contentId = popup.getId();
+
+        // when
+        LikeRequestDto requestDto = LikeRequestDto.of(contentType, contentId);
+        IntStream.range(0, threadCount).parallel().forEach(i ->
+                likeService.toggleLike(requestDto, user)
+        );
+
+        // then
+        int popupLikeCount = popupRepository.findById(contentId).orElseThrow().getLikeCount();
+        assertThat(popupLikeCount).isEqualTo(threadCount);
+    }
+
+}
